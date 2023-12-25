9947f396835c2beb1f77f0e706ad9cb7b72def07
Adeel332 <149563350+Adeel332@users.noreply.github.com>
2023-12-25 14:20:49 +0500

    Add files via upload

---
+++ homePage.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="NBA App",
+    page_icon="🏀"
+)
+st.title("NBA- National Basketball Association🏀")
+
+
+
+st.sidebar.success("Select a demo above.")
+
